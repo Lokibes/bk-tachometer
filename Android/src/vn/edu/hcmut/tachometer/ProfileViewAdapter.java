e31a8979995a40674931c93118c9794596914c03
dzprince@gmail.com <dzprince@gmail.com@c911b745-5ed8-c63a-d69a-3f07f4538a0a>
2013-04-18 19:34:29 +0000

    Complete the taking profile avatar feature (haven't test all the cases)

--- Android/src/vn/edu/hcmut/tachometer/ProfileViewAdapter.java
+++ Android/src/vn/edu/hcmut/tachometer/ProfileViewAdapter.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.preference.PreferenceManager;
@@ -66,8 +69,8 @@ class ProfileViewAdapter extends ArrayAdapter<Profile>	{
 							profileList.get(position).minRPM + " ~ " +
 							profileList.get(position).maxRPM + " RPM";
 		profileholder.summary_view.setText(summary);
-		profileholder.img_view.setImageURI(Uri.fromFile(new File(profileList.get(position).avatar)));
-		
+		//profileholder.img_view.setImageURI(Uri.fromFile(new File(profileList.get(position).avatar)));
+		profileholder.img_view.setImageBitmap(decodeSampledBitmapFromResource(profileList.get(position).avatar, 200, 200));
 		convertView.setBackgroundColor(Color.BLACK);
 		
 		if (position == currentPos)	{
@@ -77,6 +80,40 @@ class ProfileViewAdapter extends ArrayAdapter<Profile>	{
 		}
 		
 		return convertView;
+	}
+	
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+	    // Raw height and width of image
+	    final int height = options.outHeight;
+	    final int width = options.outWidth;
+	    int inSampleSize = 1;
+		
+	    if (height > reqHeight || width > reqWidth) {
+	        // Calculate ratios of height and width to requested height and width
+	        final int heightRatio = Math.round((float) height / (float) reqHeight);
+	        final int widthRatio = Math.round((float) width / (float) reqWidth);
+	
+	        // Choose the smallest ratio as inSampleSize value, this will guarantee
+	        // a final image with both dimensions larger than or equal to the
+	        // requested height and width.
+	        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+	    }
+    	
+    	return inSampleSize;
+	}
+	
+	public static Bitmap decodeSampledBitmapFromResource(String path, int reqWidth, int reqHeight) {
+	    // First decode with inJustDecodeBounds=true to check dimensions
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(path, options);
+	    
+	    // Calculate inSampleSize
+	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+	    // Decode bitmap with inSampleSize set
+	    options.inJustDecodeBounds = false;
+	    return BitmapFactory.decodeFile(path, options);
 	}
 	
 	static class ProfileHolder	{
